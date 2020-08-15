d87055b6b2012317c7d2be1ba4fdbfb496700e68
weidong Fu <weidongfu@weidongs-iMac.local>
2020-08-15 14:38:23 +0800

    add dependencies

--- IndexStoreCmd/IndexStoreCmd/main.swift
+++ IndexStoreCmd/IndexStoreCmd/main.swift
@@ -5,7 +5,9 @@
 //  Created by vedon on 2020/8/12.
 //  Copyright © 2020 vedon. All rights reserved.
 //
-
+//https://github.com/tuist/tuist
+//https://github.com/yonaskolb/XcodeGen
+//https://github.com/tuist/XcodeProj
 import Foundation
 import IndexStoreDB
 
